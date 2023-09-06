dc558ee9419b2d49876e9370752e6e7dfde40371
rajamohana <mohan.cse540@gmail.com>
2023-09-06 03:16:39 -0400

    Update user-authentication.service.spec.ts

--- src/app/services/user-authentication.service.spec.ts
+++ src/app/services/user-authentication.service.spec.ts
@@ -3,14 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { UserAuthenticationService } from './user-authentication.service';
 
 describe('UserAuthenticationService', () => {
-  let service: UserAuthenticationService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserAuthenticationService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
+    const service: UserAuthenticationService = TestBed.get(UserAuthenticationService);
     expect(service).toBeTruthy();
   });
 });
