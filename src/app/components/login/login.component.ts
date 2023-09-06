da0c3afdff7369ef279923aace4ec4ee68def618
rajamohana <mohan.cse540@gmail.com>
2023-09-06 03:10:59 -0400

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { UserAuthenticationService } from '../../services/user-authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: any;
+  errorMessage: string = "";
+
+  constructor(private httpClient: HttpClient, private userAuth: UserAuthenticationService, 
+    private  router: Router, private fb:FormBuilder) {}
+
+  ngOnInit(): void {
+    this.clearMessage();
+    if(this.userAuth.authenticated === true){      
+      this.router.navigateByUrl('/logged-user');
+    }
+    this.loginForm = this.fb.group({
+      username : new FormControl('', Validators.required),
+      password : new FormControl('', Validators.required)
+    });
+  }
+
+  clearMessage() {
+    if( this.userAuth.authenticated === false) {
+      this.errorMessage = '';
+    }
+  }
+
+  login() {
+    this.userAuth.authenticate(this.loginForm.value, () => {
+      if( this.userAuth.authenticated === true) {
+        this.router.navigateByUrl('/landing');
+      } else {
+        console.log('User not authenticated!');
+        this.errorMessage = "Incorrect Username / Password!";
+      } 
+    });
+    this.loginForm.reset();
+    return false;
+  }
 
 }
