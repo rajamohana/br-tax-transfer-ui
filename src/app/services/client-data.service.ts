5570cf97efef50629a37abfbaecb4302776850ff
rajamohana <mohan.cse540@gmail.com>
2023-09-06 03:16:00 -0400

    Update client-data.service.ts

--- src/app/services/client-data.service.ts
+++ src/app/services/client-data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class ClientDataService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+  
+  getCustomerCodes() {
+    return this.http.get('/api/listAllCustomerCodes');
+  }
+  getlistAllTaxOpenTasks(clientCode: string){
+    return this.http.get('/api/viewAllTaxOpenTasks/'+ clientCode);
+  }
+  getlistAllTaxClosedTasks(status: string, clientCode: string){
+    return this.http.get('/api/viewAllTaxClosedTasks/' + status + '/' + clientCode);
+  }
+  getTaxClaimedTaskRelease(requestBody: any) {
+    return this.http.post('/api/taxClaimedTaskRelease', requestBody);
+  }
+
+  
 }
