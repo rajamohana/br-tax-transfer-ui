796dc270422dda0549db95e0a7fa440bdf3b4d0a
rajamohana <mohan.cse540@gmail.com>
2023-09-06 11:55:58 +0530

    creating components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TasksComponent,
+    TaskDetailComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
