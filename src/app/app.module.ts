0bb256ad89cbe8a375022e673b24f20144867f01
rajamohana <mohan.cse540@gmail.com>
2023-09-11 09:59:12 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule,FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnagularMaterialModule } from './angular-material/angular-material.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ReactiveFormsModule,FormsModule} from '@angular/forms';
-import { MatButtonModule} from '@angular/material/button';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatIconModule} from '@angular/material/icon';
-import { MatMenuModule} from '@angular/material/menu';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import { MatSelectModule} from '@angular/material/select';
-import { MatExpansionModule} from '@angular/material/expansion';
-import { MatRadioModule} from '@angular/material/radio';
-import { MatCheckboxModule} from '@angular/material/checkbox';
-import { MatPaginatorModule} from '@angular/material/paginator';
-import { MatTableModule} from '@angular/material/table';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule} from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './components/landing/landing.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { ConfigService } from './services/config.service';
+
 
 
 @NgModule({
@@ -38,31 +29,20 @@ import { LoginComponent } from './components/login/login.component';
     TasksComponent,
     TaskDetailComponent,
     LoginComponent,
+    NavComponent,
+    FormsComponent,
+    ReportsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AnagularMaterialModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-    MatExpansionModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatPaginatorModule,
-    MatCheckboxModule,
-    MatTableModule,
-    MatDialogModule,
-    MatCardModule,
-    MatTabsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule,
-    
+    FormsModule
   ],
-  providers: [],
+  providers: [ConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
